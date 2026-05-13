5b51d8ef805b458d2df2a734242e78b6e4f09956
DEVNALSOFT <mobiledevnalsoft@gmail.com>
2026-05-13 22:20:51 +0530

    chore: delete maddyOrganise.ts redundant re-export wrapper

--- convex/maddyOrganise.ts
+++
@@ -1,6 +0,0 @@
-import { v } from "convex/values";
-import { action } from "./_generated/server";
-import { internal, api } from "./_generated/api";
-
-// Re-export from maddy.ts for backwards compatibility
-export { organiseWorkspace } from "./maddy";
